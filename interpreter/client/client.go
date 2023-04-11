0c3bb3875e72fce32130bd01db5a6ff6d1e4fb1b
yul <1103221645@qq.com>
2023-04-11 20:27:58 +0800

    token拦截

--- interpreter/client/client.go
+++ interpreter/client/client.go
@@ -4,17 +4,27 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"micro-server/interpreter/proto"
 )
 
 func main() {
 	interceptor := func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 		fmt.Println("发送前拦截")
+		// 同一添加身份信息
+		md := metadata.New(map[string]string{
+			"appid":  "1010",
+			"appkey": "huang",
+		})
+		ctx = metadata.NewOutgoingContext(context.Background(), md)
 		return invoker(ctx, method, req, reply, cc, opts...)
 	}
 	opt := grpc.WithUnaryInterceptor(interceptor)
 	conn, _ := grpc.Dial("0.0.0.0:5555", grpc.WithInsecure(), opt)
 	client := proto.NewGreeterClient(conn)
-	r, _ := client.Hello(context.Background(), &proto.ServerRequest{Name: "huang"})
+	r, err := client.Hello(context.Background(), &proto.ServerRequest{Name: "huang"})
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println(r)
 }
--- interpreter/server/server.go
+++ interpreter/server/server.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"micro-server/interpreter/proto"
 	"net"
 )
@@ -15,9 +19,34 @@ func (s *Server) Hello(ctx context.Context, req *proto.ServerRequest) (*proto.Se
 }
 
 func main() {
-	listen, _ := net.Listen("tcp", ":5555")
+	interceptor := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		fmt.Println("拦截器响应")
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			return resp, status.Error(codes.Unauthenticated, "没有验证信息")
+		}
+		// 从metadata中取出验证信息
+		var (
+			appid  string
+			appkey string
+		)
+		if va1, ok := md["appid"]; ok {
+			appid = va1[0]
+		}
+		if va2, ok := md["appkey"]; ok {
+			appkey = va2[0]
+		}
+		if appid != "10109" || appkey != "huang" {
+			return resp, status.Error(codes.Unauthenticated, "信息不正确")
+		}
+		// 原样执行
+		return handler(ctx, req)
+	}
+	// 添加拦截器
+	opt := grpc.UnaryInterceptor(interceptor)
+	listen, _ := net.Listen("tcp", "127.0.0.1:5555")
 
-	g := grpc.NewServer()
+	g := grpc.NewServer(opt) // 多个拦截器  opt,opt2,opt3
 	proto.RegisterGreeterServer(g, &Server{})
 	_ = g.Serve(listen)
 }
