3ed98792c9ec24e724809e173a99dc69e7d1c6c4
yul <1103221645@qq.com>
2023-04-11 19:49:54 +0800

    拦截器

--- interpreter/client/client.go
+++ interpreter/client/client.go
@@ -8,7 +8,12 @@ import (
 )
 
 func main() {
-	conn, _ := grpc.Dial("0.0.0.0:5555", grpc.WithInsecure())
+	interceptor := func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		fmt.Println("发送前拦截")
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
+	opt := grpc.WithUnaryInterceptor(interceptor)
+	conn, _ := grpc.Dial("0.0.0.0:5555", grpc.WithInsecure(), opt)
 	client := proto.NewGreeterClient(conn)
 	r, _ := client.Hello(context.Background(), &proto.ServerRequest{Name: "huang"})
 	fmt.Println(r)
